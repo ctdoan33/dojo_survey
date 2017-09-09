70c4dd64c8af0745af6ed7c6d0aea086cb39db3d
ctdoan33 <doanchristopher_1@yahoo.com>
2017-09-08 20:58:49 -0700

    added some validation

--- server.py
+++ server.py
@@ -1,13 +1,20 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, redirect, request, session, flash
 app = Flask(__name__)
+app.secret_key = 'KeepItSecretKeepItSafe'
 @app.route("/")
 def survey():
     return render_template("index.html")
 @app.route("/result", methods=["POST"])
 def results():
     name = request.form["name"]
+    if len(name)<1:
+        flash("Name field cannot be blank")
     location = request.form["location"]
     language = request.form["language"]
     comment = request.form["comment"]
+    if len(comment)<1:
+        flash("Comment field cannot be blank")
+    elif len(comment)>120:
+        flash("Comment cannot be more than 120 characters")
     return render_template("result.html", subname=name, sublocation=location, sublanguage=language, subcomment=comment)
 app.run(debug=True)
